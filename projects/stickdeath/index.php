4891a0461511d27e0ed25c156df12f6bcc46139f
Oliver Zheng <gitcommits@oliverzheng.com>
2010-02-05 12:24:12 -0800

    More projects.

---
+++ projects/stickdeath/index.php
@@ -0,0 +1,19 @@
+<HTML>
+<HEAD>
+<TITLE>Stick Death</TITLE>
+</HEAD>
+<BODY bgcolor="#FFFFFF" leftmargin="0" rightmargin="0" topmargin="0" bottommargin="0">
+<OBJECT classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=5,0,0,0" WIDTH=600 HEIGHT=400><PARAM NAME=movie VALUE="./stickdeath.swf"><PARAM NAME=menu VALUE=false><PARAM NAME=quality VALUE=best><PARAM NAME=bgcolor VALUE=#FFFFFF><EMBED src="./stickdeath.swf" menu=false quality=best bgcolor=#FFFFFF WIDTH=600 HEIGHT=400 TYPE="application/x-shockwave-flash" PLUGINSPAGE="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash"></EMBED>
+</OBJECT>
+</BODY>
+</HTML>
+
+
+
+
+
+
+
+
+
+
